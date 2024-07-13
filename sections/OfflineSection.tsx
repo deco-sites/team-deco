042f5d23c39965cad9956fcae38348eebbf1030a
Deepak Vaishnav <deepak.divaishnav1996@gmail.com>
2024-07-13 10:49:32 +0000

    Release ayushsinghal123321 environment with 0 changes made by

---
+++ sections/OfflineSection.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  /**
+  * @description The description of name.
+  */
+  name?: string;
+}
+
+export default function Section({ name = "" }: Props) {
+  return (
+    <div>
+        
+    </div>
+  )
+}
