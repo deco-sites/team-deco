f9222136dfa37d94fa54717abc230015f8f3f452
Udit Agrawal <agrawaludit31@gmail.com>
2024-07-13 15:45:25 +0000

    Release mojo environment with 0 changes made by

---
+++ sections/LoginSection.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  /**
+  * @description The description of name.
+  */
+  name?: string;
+}
+
+export default function Section({ name = "Capy" }: Props) {
+  return (
+    <div cl>
+
+    </div>
+  )
+}
