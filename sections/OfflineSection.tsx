a6418a015e3b45882c88efe37adc5c500e2654d3
Deepak Vaishnav <deepak.divaishnav1996@gmail.com>
2024-07-13 15:32:45 +0000

    Release mojo environment with 2 changes made by Deepak Vaishnav

--- sections/OfflineSection.tsx
+++ sections/OfflineSection.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Section({ name = "Something went wrong!!" }: Props) {
   return (
     <div className="flex items-center justify-center">
-      <h1 className="text-2xl mt-2 font-semibold text-red-400">
+      <h1 className="text-2xl mt-2 font-semibold text-red-400 mt-100">
         {name}
       </h1>
     </div>
